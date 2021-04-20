8e00f31448568367bb4584a6e6c24fa53b0849a6
Syed Yashin Hussain <syedeyashin@gmail.com>
2021-04-20 21:55:27 +0530

    added firebase.js to misc to config the app

---
+++ src/misc/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+
+const config = {
+  apiKey: "AIzaSyClhMh7oOh8DWygQlErIfL2bA6ZdwQhZbM",
+  authDomain: "chat-app-97590.firebaseapp.com",
+  databaseURL: "https://chat-app-97590-default-rtdb.firebaseio.com",
+  projectId: "chat-app-97590",
+  storageBucket: "chat-app-97590.appspot.com",
+  messagingSenderId: "687604172245",
+  appId: "1:687604172245:web:3e30924a7e4bba15dae95c",
+};
+const app = firebase.initializeApp(config);
